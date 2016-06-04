47034c4d85278f296e26f11e872a2dca9c0f2b25
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2016-06-05 00:12:51 +0200

    Update input.h

--- input.h
+++ input.h
@@ -17,8 +17,8 @@ void GUI_Input_SetTextOffset(GUI_Input i, unsigned int text_offset);
 void GUI_Input_SetPlaceholder(GUI_Input i, const char * placeholder);
 void GUI_Input_SetPositionFromAnchor(GUI_Input i, int x, int y, GUI_Anchor anchor);
 void GUI_Input_SetTextFont(GUI_Input i, const char * font_path, unsigned int font_size);
-void GUI_Input_SetHoverColor(GUI_Input i, const char * border_color, const char * button_color);
-void GUI_Input_SetNormalColor(GUI_Input i, const char * border_color, const char * button_color);
+void GUI_Input_SetHoverColor(GUI_Input i, const char * border_color, const char * background_color);
+void GUI_Input_SetNormalColor(GUI_Input i, const char * border_color, const char * background_color);
 void GUI_Input_SetText(GUI_Input i, const char * text, const char * font_path, unsigned int font_size);
 
 const char * GUI_InputGetValue(GUI_Input i);
